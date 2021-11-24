c85212eed5f6a6c0cbbdbe3c6ec7387d6f33012f
rgoshen <rick.goshen@gmail.com>
2021-11-24 16:28:39 -0700

    finished error_checks

---
+++ error_checks.py
@@ -0,0 +1,28 @@
+from forex_python.converter import CurrencyRates
+
+
+def check_valid_amount(val):
+    '''Checking for a valid value.  Returns ValueError if not valid else returns val.'''
+
+    if not val or not val.isnumeric():
+        raise ValueError('Not a valid amount')
+
+    return val
+
+
+def check_valid_code(code):
+    '''Checks for a valid code. Returns ValueError if not valid else returns code.'''
+
+    cr = CurrencyRates()
+    code = code.upper()
+
+    # if USD it is a valid code
+    if(code == 'USD'):
+        return code
+
+    all_currencies = list(cr.get_rates('USD').keys())
+
+    if(not code or code not in all_currencies):
+        raise ValueError('Not a valid code')
+
+    return code
