98cc3ef489af023d5fbd6a0ffc153933cbe317b6
rgoshen <rick.goshen@gmail.com>
2021-11-23 22:18:52 -0700

    completed routes and result page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from currency_rates import get_currency_symbol, convert_currency
 
 app = Flask(__name__)
 
@@ -6,3 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def get_from():
     return render_template("index.html")
+
+
+@app.route('/convert', methods=['GET', 'POST'])
+def get_currency_converted():
+    if request.method == "POST":
+        from_currency = request.form.get('from')
+        to_currency = request.form.get('to')
+        amount = request.form.get('amount')
+        to_sym = get_currency_symbol(to_currency)
+        result = '{:.2f}'.format(convert_currency(
+            from_currency, to_currency, amount))
+        return render_template('result.html', result=result, to_sym=to_sym)
+    return redirect('/')
---
+++ currency_rates.py
@@ -0,0 +1,21 @@
+from forex_python.converter import CurrencyCodes, CurrencyRates
+
+cr = CurrencyRates()
+cc = CurrencyCodes()
+
+all_currencies = list(cr.get_rates('USD').keys())
+
+
+def get_conversion_rate(from_curr, to_curr):
+    '''Returns the conversion rate.'''
+    return cr.get_rate(from_curr.upper(), to_curr.upper())
+
+
+def get_currency_symbol(code):
+    '''Returns the currency symbol.'''
+    return cc.get_symbol(code.upper())
+
+
+def convert_currency(from_curr, to_curr, amount=0):
+    '''Returns the converted amount.'''
+    return cr.convert(from_curr.upper(), to_curr.upper(), float(amount))
