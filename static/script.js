fb9891591c3e596bb3dc1d3dcf942ad9e35589e3
rgoshen <rick.goshen@gmail.com>
2021-11-24 19:52:01 -0700

    added basic footer

---
+++ static/script.js
@@ -0,0 +1,5 @@
+const currentYear = document.getElementById("current-year");
+
+// update copyright date to current year
+const year = new Date().getFullYear();
+currentYear.innerHTML = year.toString();
